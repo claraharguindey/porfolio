ced76669ae045db641fea73e5868b1923e1a0527
clara <clarisaharguindey@gmail.com>
2019-02-25 22:05:45 +0100

    update about

--- src/About/index.js
+++ src/About/index.js
@@ -4,8 +4,17 @@ import './About.scss';
 class About extends Component {
     render() { 
         return (
-            <p>about</p>
-          );
+            <p className="about__text">Mi interés por la <span className="special__word">tecnología</span> y el <span className="special__word">arte</span> me ha llevado a trabajar durante
+            tres años desarrollando proyectos de <span className="special__word">mediación cultural</span> basados en
+            estrategias digitales y de <span className="special__word">gamificación</span>.
+            Trabajar con equipos multidisciplinares, formados por programadores,
+            artistas y educadores, ha mejorado mi asertividad y creatividad en la
+            búsqueda de soluciones, procurando la cohesión de intereses
+            individuales para lograr objetivos comunes.
+            Tras formarme en Adalab, he descubierto que programar me resulta
+            apasionante. Ahora busco formar parte de un proyecto con el que
+            comprometerme y seguir creciendo como desarrolladora.</p>
+        );
     }
 }
  
